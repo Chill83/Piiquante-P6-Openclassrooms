b586f5d37e3260a2ee112cc05b73ded55d28401f
Chill83 <aymericmaggio@gmail.com>
2022-08-15 15:11:59 +0200

    Add helmet + dotenv

--- app.js
+++ app.js
@@ -3,25 +3,31 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const helmet = require('helmet');
+const dotenv = require('dotenv').config();
 
 // Importation routes
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
 
 // Connection et authentification à MongoDB (base de données)
-mongoose.connect('mongodb+srv://Aymeric:projet6openclassrooms@cluster0.tr0qzir.mongodb.net/?retryWrites=true&w=majority',
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER_NAME}.mongodb.net/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-// Initialisation variables et constantes
+// J'applique express
 const app = express();
 
-app.use(express.json());
+// Utilisation Helmet
+app.use(helmet());
 
 // Configuration CORS
 app.use(cors());
+
+// Reconnaitre les requêtes en objets JSON
+app.use(express.json());
 
 // Gestionnaire de routage image
 app.use('/images', express.static(path.join(__dirname, 'images')));
