519e2a2f8613c1a875284a5804d48a966e2a73b0
Chill83 <aymericmaggio@gmail.com>
2022-08-22 14:32:34 +0200

    Added passwordvalidator package for signup route

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,10 @@ const express = require('express');
 const router = express.Router();
 // Importation modules
 const userCtrl = require('../controllers/user');
+const passwordValidator = require('../middlewares/password-validator');
 
 // Routes
-router.post('/signup', userCtrl.signup);
+router.post('/signup', passwordValidator, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 // Exportation modules
